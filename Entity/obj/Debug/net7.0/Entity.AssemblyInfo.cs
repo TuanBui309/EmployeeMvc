f23a0b938d1aaed3d5aaf4fe2107475e30363137
Bui_Van_Tuan <nangbanmai3092001@gmail.com>
2025-05-05 10:17:28 +0700

    update

--- Entity/obj/Debug/net7.0/Entity.AssemblyInfo.cs
+++ Entity/obj/Debug/net7.0/Entity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+514fe2e8b92962182a0013b2450db4a2af88ef64")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
