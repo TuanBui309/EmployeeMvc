fe5277b705d8fdaf2ef3660bfcb0ffb557badf7d
Bui_Van_Tuan <nangbanmai3092001@gmail.com>
2025-06-06 09:26:24 +0700

    update_const

--- Entity/Constants/MessageConstants.cs
+++ Entity/Constants/MessageConstants.cs
@@ -2,19 +2,19 @@
 
 public static class MessageConstants
 {
-    public static readonly string BadRequest = "Yêu cầu không hợp lệ!";
-    public static readonly string Unauthorized = "Không có quyền truy cập!";
-    public static readonly string Forbidden = "Không đủ quyền truy cập!";
-    public static readonly string NotFound = "Không tìm thấy tài nguyên!";
-    public static readonly string InternalServerError = "Dữ liệu không hợp lệ!";
-    public static readonly string Success = "Xử lý thành công!";
-    public static readonly string InsertSuccess = "Thêm mới thành công!";
-    public static readonly string InsertFailure = "Thêm mới thất bại!";
-    public static readonly string UpdateSuccess = "Cập nhật thành công!";
-    public static readonly string UpdateFailure = "Cập nhật thất bại!";
-    public static readonly string DeleteSuccess = "Xóa thành công!";
-    public static readonly string DeleteFailure = "Xóa thất bại!";
-    public static readonly string TokenGenerationFailure = "Không thể tạo token!";
-    public static readonly string UploadSuccess = "Upload thành công!";
-    public static readonly string UploadFailure = "Upload thất bại!";
+    public const string BadRequest = "Yêu cầu không hợp lệ!";
+    public const string Unauthorized = "Không có quyền truy cập!";
+    public const string Forbidden = "Không đủ quyền truy cập!";
+    public const string NotFound = "Không tìm thấy tài nguyên!";
+    public const string InternalServerError = "Dữ liệu không hợp lệ!";
+    public const string Success = "Xử lý thành công!";
+    public const string InsertSuccess = "Thêm mới thành công!";
+    public const string InsertFailure = "Thêm mới thất bại!";
+    public const string UpdateSuccess = "Cập nhật thành công!";
+    public const string UpdateFailure = "Cập nhật thất bại!";
+    public const string DeleteSuccess = "Xóa thành công!";
+    public const string DeleteFailure = "Xóa thất bại!";
+    public const string TokenGenerationFailure = "Không thể tạo token!";
+    public const string UploadSuccess = "Upload thành công!";
+    public const string UploadFailure = "Upload thất bại!";
 }
--- Entity/Constants/StatusCodeConstants.cs
+++ Entity/Constants/StatusCodeConstants.cs
@@ -2,21 +2,21 @@
 
 public static class StatusCodeConstants
 {
-    public static readonly int InternalServerError = 500;
-    public static readonly int BadRequest = 400;
-    public static readonly int NotFound = 404;
-    public static readonly int Unauthorized = 401;
-    public static readonly int Forbidden = 403;
-    public static readonly int Created = 201;
-    public static readonly int Ok = 200;
+    public const int InternalServerError = 500;
+    public const int BadRequest = 400;
+    public const int NotFound = 404;
+    public const int Unauthorized = 401;
+    public const int Forbidden = 403;
+    public const int Created = 201;
+    public const int Ok = 200;
 
 }
 public static class Validations
 {
-    public static readonly int MaximumNumberOfDegrees = 3;
-    public static readonly int NameMaxLength = 250;
-    public static readonly int MinAge = 0;
-    public static readonly int MaxAge = 150;
-    public static readonly int NameDegreeMaxLength = 250;
-    public static readonly int IssuedByMaxLength = 550;
+    public const int MaximumNumberOfDegrees = 3;
+    public const int NameMaxLength = 250;
+    public const int MinAge = 0;
+    public const int MaxAge = 150;
+    public const int NameDegreeMaxLength = 250;
+    public const int IssuedByMaxLength = 550;
 }
